96a9e60b10e14f67411e928785e2d81715f8657a
Thomas N. Jensen <thomasnorupjensen@gmail.com>
2024-02-15 20:30:38 +0100

    Edited layout responsive

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,13 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../footer/Footer";
 import Header from "../header/Header";
+import styles from './layout.module.css';
 
 
 // The <Outlet> from react-router-dom displays any child routes, almost like
 // passing through "children" in a component
 function Layout(){
     return(
-        <div>
+        <div className={styles.wrapper}>
             <Header />
             <Outlet />
             <Footer />
